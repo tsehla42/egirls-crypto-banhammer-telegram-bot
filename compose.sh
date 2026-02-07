739fd57927de39d894fc19ad161dea60997e20b3
tsehla42 <tsehla@duck.com>
2026-02-07 19:05:29 +0200

    fix .compose.sh

--- compose.sh
+++ compose.sh
@@ -4,6 +4,9 @@ set -euo pipefail
 IMAGE=egirls-banhammer-bot
 PROJECT=egirls-banhammer-bot
 
+mkdir -p logs data
+sudo chown -R 1000:1000 logs data
+
 docker build -t "$IMAGE" .
 
 COMPOSE_PROJECT_NAME=$PROJECT docker compose up -d
